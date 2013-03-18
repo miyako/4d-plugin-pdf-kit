3b70a4aae084680ef6a6e22fd7844524c65b4118
miyako <keisuke.miyako@4d.com>
2013-03-18 22:23:16 +0900

    fixed utf16-to-utf8 routine for mac in C_TEXT, ARRAY_TEXT

--- PDF Kit/4D Plugin API/Classes/ARRAY_TEXT.cpp
+++ PDF Kit/4D Plugin API/Classes/ARRAY_TEXT.cpp
@@ -82,37 +82,58 @@ void ARRAY_TEXT::toParamAtIndex(PackagePtr pParams, uint32_t index)
 
 void ARRAY_TEXT::convertFromUTF8(const CUTF8String* fromString, CUTF16String* toString)	
 {
-	uint32_t size = ((uint32_t)fromString->length() * sizeof(PA_Unichar)) + sizeof(PA_Unichar);
-	std::vector<uint8_t> buf(size);
+#ifdef _WIN32
+	int len = MultiByteToWideChar(CP_UTF8, 0, (LPCSTR)fromString->c_str(), fromString->length(), NULL, 0);
 	
-	uint32_t len = PA_ConvertCharsetToCharset(
-											  (char *)fromString->c_str(),
-											  (uint32_t)fromString->length(),
-											  eVTC_UTF_8,
-											  (char *)&buf[0],
-											  size,
-											  eVTC_UTF_16
-											  );
-	
-//	*toString = CUTF16String((const PA_Unichar *)&buf[0], len);	
-	*toString = CUTF16String((const PA_Unichar *)&buf[0], len/2);	
+	if(len){
+		std::vector<uint8_t> buf((len + 1) * sizeof(PA_Unichar));
+		if(MultiByteToWideChar(CP_UTF8, 0, (LPCSTR)fromString->c_str(), fromString->length(), (LPWSTR)&buf[0], len)){
+			*toString = CUTF16String((const PA_Unichar *)&buf[0]);
+		}
+    }else{
+			*toString = CUTF16String((const PA_Unichar *)L"");
+	}
+           
+#else
+           CFStringRef str = CFStringCreateWithBytes(kCFAllocatorDefault, fromString->c_str(), fromString->length(), kCFStringEncodingUTF8, true);
+           if(str){
+               int len = CFStringGetLength(str);
+               std::vector<uint8_t> buf((len+1) * sizeof(PA_Unichar));
+               CFStringGetCharacters(str, CFRangeMake(0, len), (UniChar *)&buf[0]);
+               *toString = CUTF16String((const PA_Unichar *)&buf[0]);
+               CFRelease(str);
+           }
+#endif	
 }
 
 void ARRAY_TEXT::convertToUTF8(const CUTF16String* fromString, CUTF8String* toString)
 {
-	uint32_t size = ((uint32_t)fromString->length() * 4) + sizeof(uint8_t);
-	std::vector<uint8_t> buf(size);
+#ifdef _WIN32
+	int len = WideCharToMultiByte(CP_UTF8, 0, (LPCWSTR)fromString->c_str(), fromString->length(), NULL, 0, NULL, NULL);
 	
-	uint32_t len = PA_ConvertCharsetToCharset(
-											  (char *)fromString->c_str(),
-											  (uint32_t)fromString->length() * sizeof(PA_Unichar),
-											  eVTC_UTF_16,
-											  (char *)&buf[0],
-											  size,
-											  eVTC_UTF_8
-											  );
-	
-	*toString = CUTF8String((const uint8_t *)&buf[0], len);
+	if(len){
+		std::vector<uint8_t> buf(len + 1);
+		if(WideCharToMultiByte(CP_UTF8, 0, (LPCWSTR)fromString->c_str(), fromString->length(), (LPSTR)&buf[0], len, NULL, NULL)){
+			*toString = CUTF8String((const uint8_t *)&buf[0]);
+		}
+	}else{
+			*toString = CUTF8String((const uint8_t *)"");
+	}
+           
+#else
+           CFStringRef str = CFStringCreateWithCharacters(kCFAllocatorDefault, (const UniChar *)fromString->c_str(), fromString->length());
+           if(str){
+               
+               size_t size = CFStringGetMaximumSizeForEncoding(CFStringGetLength(str), kCFStringEncodingUTF8) + sizeof(uint8_t);
+               std::vector<uint8_t> buf(size);
+               CFIndex len = 0;
+               CFStringGetBytes(str, CFRangeMake(0, CFStringGetLength(str)), kCFStringEncodingUTF8, 0, true, (UInt8 *)&buf[0], size, &len);
+               
+               *toString = CUTF8String((const uint8_t *)&buf[0], len);	
+               CFRelease(str);
+           }	
+           
+#endif
 }
 
 void ARRAY_TEXT::copyUTF8StringAtIndex(CUTF8String* pString, uint32_t index)
@@ -139,6 +160,22 @@ void ARRAY_TEXT::setUTF16StringAtIndex(const PA_Unichar* pString, uint32_t len, 
 	this->setUTF16StringAtIndex(&u, index);
 }
 
+
+
+
+void ARRAY_TEXT::copyPathAtIndex(CUTF8String* pString, uint32_t index)
+{
+#if VERSIONMAC
+#ifdef __OBJC__	
+	NSString *path = this->copyPathAtIndex(index);
+	*pString = CUTF8String((const uint8_t *)[path UTF8String]);
+	[path release];
+#endif
+#else
+	this->copyUTF8StringAtIndex(pString, index);
+#endif		
+}
+	
 void ARRAY_TEXT::setUTF16StringAtIndex(CUTF16String* pString, uint32_t index)
 {
 	if(index < this->_CUTF16StringArray->size())
@@ -205,7 +242,7 @@ void ARRAY_TEXT::appendUTF16String(const PA_Unistring* pString)
 
 #if VERSIONMAC
 #ifdef __OBJC__	
-void ARRAY_TEXT::appendUTF16String(const NSString* pString)
+void ARRAY_TEXT::appendUTF16String(NSString* pString)
 {	
 	uint32_t len = [pString length];
 	uint32_t size = (len * sizeof(PA_Unichar)) + sizeof(PA_Unichar);
@@ -228,6 +265,41 @@ void ARRAY_TEXT::setUTF16StringAtIndex(NSString* pString, uint32_t index)
 	
 	if([pString getCString:(char *)&buf[0] maxLength:size encoding:NSUnicodeStringEncoding])
 		this->setUTF16StringAtIndex((const PA_Unichar *)&buf[0], len, index);	
+}
+
+NSString *ARRAY_TEXT::copyUTF16StringAtIndex(uint32_t index)
+{
+	CUTF16String strUtf16;
+	
+	strUtf16 = CUTF16String(this->_CUTF16StringArray->at(index));
+	
+	return [[NSString alloc]initWithCharacters:strUtf16.c_str() length:strUtf16.length()];
+}
+
+NSString *ARRAY_TEXT::copyPathAtIndex(uint32_t index){
+	
+	NSString *path = @"";
+	
+	NSURL *u = this->copyUrlAtIndex(index);
+	
+	if(u){
+		path = (NSString *)CFURLCopyFileSystemPath((CFURLRef)u, kCFURLPOSIXPathStyle);
+		[u release];
+	}
+	
+	return path;
+	
+}		
+NSURL *ARRAY_TEXT::copyUrlAtIndex(uint32_t index){
+	
+	NSURL *u = NULL;
+	
+	NSString *str = this->copyUTF16StringAtIndex(index);
+	u = (NSURL *)CFURLCreateWithFileSystemPath(kCFAllocatorDefault, (CFStringRef)str, kCFURLHFSPathStyle, false);
+	[str release];
+	
+	return u;
+	
 }
 #endif
 #endif
